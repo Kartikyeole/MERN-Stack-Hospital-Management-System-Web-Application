71e22c2bad7470148541181821e85e48248620d5
Kartikyeole <82437231+Kartikyeole@users.noreply.github.com>
2024-04-25 12:46:15 +0530

    Update Biography.jsx

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -11,27 +11,17 @@ const Biography = ({imageUrl}) => {
           <p>Biography</p>
           <h3>Who We Are</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            blanditiis sequi aperiam. Debitis fugiat harum ex maxime illo
-            consequatur mollitia voluptatem omnis nihil nesciunt beatae esse
-            ipsam, sapiente totam aspernatur porro ducimus aperiam nisi. Ex
-            magnam voluptatum consectetur reprehenderit fugiat recusandae aut
-            similique illum natus velit, praesentium nostrum nesciunt. Deleniti,
-            nesciunt laboriosam totam iusto!
+          Zeecare is a leading online healthcare platform based in Baner, Pune. We connect patients with experienced doctors for virtual consultations and offer a range of lab testing services. With a focus on convenience and quality care, Zeecare brings healthcare to your doorstep, making it easier to access medical advice and test results.
+
+
+
+
+
+
+
           </p>
-          <p>We are all in 2024!</p>
-          <p>We are working on a MERN STACK PROJECT.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            assumenda exercitationem accusamus sit repellendus quo optio dolorum
-            corporis corrupti. Quas similique vel minima veniam tenetur
-            obcaecati atque magni suscipit laboriosam! Veniam vitae minus nihil
-            cupiditate natus provident. Ex illum quasi pariatur odit nisi
-            voluptas illo qui ipsum mollitia. Libero, assumenda?
-          </p>
-          <p>Lorem ipsum dolor sit amet!</p>
-          <p>Coding is fun!</p>
-        </div>
+          
+                 </div>
       </div>
     </>
   );
